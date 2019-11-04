c5143b1fdeb94d9c255ccbddf626c63fa199a3e0
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-11-04 11:00:19 +0100

    Update

--- BiblioteksApp/BibilioteksApp/BibilioteksApp/Program.cs
+++ BiblioteksApp/BibilioteksApp/BibilioteksApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BibilioteksApp
 {
@@ -6,13 +7,59 @@ namespace BibilioteksApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej, Velkommen til vores Bibiliotek.");
-            Console.WriteLine("1. Låne en bog.");
-            Console.WriteLine("2. Forlænge en låneperiode.");
-            Console.WriteLine("3. Tjekke om vi har en bestemt bog.");
-            Console.WriteLine("4. Aflever en bog.");
-            Console.Write("Vælg hvad du vil: ");
-            string reply = Console.ReadLine();
+            string[] allBooks = {"Bog 1", "Bog 2", "Bog 3", "Bog 4", "Bog 5"};
+            string reply;
+            bool isRunning1 = true;
+            bool isRunning2 = true;
+            while (isRunning1 == true) 
+            { 
+                Console.WriteLine("Hej, Velkommen til vores Bibiliotek.");
+                Console.WriteLine();
+                Console.WriteLine("0. Forlade vores Bikiliotek.");
+                Console.WriteLine("1. Låne en bog.");
+                Console.WriteLine("2. Forlænge en låneperiode.");
+                Console.WriteLine("3. Tjekke om vi har en bestemt bog.");
+                Console.WriteLine("4. Aflever en bog.");
+                Console.WriteLine();
+                Console.Write("Vælg hvad du vil: ");
+                reply = Console.ReadLine();
+                Console.Clear();
+
+                if (reply == "0")
+                {
+                    Console.WriteLine("Tak fordi du gad at besøge os. Hav en god dag!");
+                    isRunning = false;
+                }
+                else if (reply == "1") 
+                {
+                    Console.WriteLine("Du har valgt at låne en bog.");
+                    Console.WriteLine();
+                    Console.WriteLine("0. Tilbage til menuen.");
+                    Console.WriteLine("1. Søge efter den.");
+                    Console.WriteLine("2. Se en liste over alle vores bøger.");
+                    Console.WriteLine();
+                    Console.Write("Hvordan vil du finde den bog du gerne vil låne: ");
+                    reply = Console.ReadLine();
+                    Console.Clear();
+
+                    while (isRunning2 == true)
+                    {
+                        if (reply == "0")
+                        {
+                            isRunning2 = false;
+                        }
+                        else if (reply = "1")
+                        {
+                            Console.Write("Skriv navnet på den bog du søger: ");
+                            reply = Console.ReadLine();
+                        }
+                        else if (reply ="2")
+                        {
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
