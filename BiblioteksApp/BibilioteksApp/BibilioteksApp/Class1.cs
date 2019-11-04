3f3ae142165b0f78f489aa1bb562c91f5fbcd1f8
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-11-04 14:49:16 +0100

    Update

---
+++ BiblioteksApp/BibilioteksApp/BibilioteksApp/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibilioteksApp
+{
+    class Library
+    {
+        public string name;
+        public string owner;
+        public Employer employer;
+        public List<Book> books;
+        public Customer customer;
+
+        List<Book> listOffBooks = new List<Book>();
+
+        public List<Book> addBook(Book book, List<Book> books)
+        {
+            books.Add(book);
+            return books;
+        }
+        public List<Book> removeBook(int bookNum, List<Book> books)
+        {
+            books.RemoveAt(bookNum);
+            return books;
+        }
+    }
+    class Book
+    {
+        public string name;
+        public string author;
+        public string releaseDate;
+        public string theme;
+        public string pages; 
+    }
+    class Employer
+    {
+        public string name;
+        public int age;
+        public int salary;
+        public string employmentDate;
+        public int employmentID;
+        public string rank;
+    }
+    class Customer
+    {
+        public string name;
+        public int age;
+        public int customerID; 
+    }
+}
