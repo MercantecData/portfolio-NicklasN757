bb34f3548bf135fa9375d6a3f7dacd2915c21100
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-11-04 10:22:02 +0100

    Start på BikilioteksApp

---
+++ BiblioteksApp/BibilioteksApp/BibilioteksApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BibilioteksApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, Velkommen til vores Bibiliotek.");
+            Console.WriteLine("1. Låne en bog.");
+            Console.WriteLine("2. Forlænge en låneperiode.");
+            Console.WriteLine("3. Tjekke om vi har en bestemt bog.");
+            Console.WriteLine("4. Aflever en bog.");
+            Console.Write("Vælg hvad du vil: ");
+            string reply = Console.ReadLine();
+        }
+    }
+}
