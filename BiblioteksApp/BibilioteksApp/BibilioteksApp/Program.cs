eb3721df31295ded7d7cb2edf1484e0a4f17c5a8
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-11-06 15:02:54 +0100

    Update fixes #2 fixes #4

--- BiblioteksApp/BibilioteksApp/BibilioteksApp/Program.cs
+++ BiblioteksApp/BibilioteksApp/BibilioteksApp/Program.cs
@@ -8,8 +8,28 @@ namespace BibilioteksApp
     {
         static void Main(string[] args)
         {
-            Library library = new Library(); 
+            Library library = new Library();
+
+            library = adder(library);
+
+            Console.WriteLine("Velkommen til " + library.name + ".");
+            Console.WriteLine("Eget af " + library.owner + ".");
+            Console.WriteLine();
+
+            printBooks(library);
+
+            Console.WriteLine();
+
+            printCustomer(library);
+
+            Console.WriteLine();
+
+            printEmployer(library);
             
+        }
+
+        public static Library adder(Library library)
+        {
             library.books.Add(new Book("Morgensjernen", "Lone Kærgård", "08.09.15", "Action", 234));
             library.books.Add(new Book("Den Lille Drage", "Søren Bogorm", "01.03.02", "Børnebog", 25));
             library.books.Add(new Book("Den Sorte regn", "Peter Sørensen", "02.06.10", "Krimi", 111));
@@ -17,7 +37,7 @@ namespace BibilioteksApp
             library.customer.Add(new Customer("Nick", 19, 2598));
             library.customer.Add(new Customer("Jack", 2, 6941));
             library.customer.Add(new Customer("Mikkel", 30, 6486));
-            
+
             library.employer.Add(new Employer("Mia", 62, 185, 66666, "Bikbitikar"));
             library.employer.Add(new Employer("Karen", 50, 185, 68481, "Bikbitikar"));
             library.employer.Add(new Employer("Sofia", 42, 215, 68438, "Leder"));
@@ -25,9 +45,10 @@ namespace BibilioteksApp
             library.name = "Mathias Bio lol";
             library.owner = "Mathias";
 
-            Console.WriteLine("Velkommen til " + library.name + ".");
-            Console.WriteLine("Eget af " + library.owner + ".");
-            Console.WriteLine();
+            return library;
+        }
+        public static void printBooks(Library library)
+        {
             Console.WriteLine("Vores udvalg af bøger består af:");
             Console.WriteLine("<>------------------------------------<>");
 
@@ -39,7 +60,9 @@ namespace BibilioteksApp
                 Console.WriteLine("<>------------------------------------<>");
                 i += 1;
             }
-            Console.WriteLine();
+        }
+        public static void printCustomer(Library library)
+        {
             Console.WriteLine("Her er en liste over alle vores kunder:");
             int s = 0;
             while (s < library.customer.Count)
@@ -47,9 +70,11 @@ namespace BibilioteksApp
                 Console.WriteLine(library.customer[s].name);
                 s += 1;
             }
-            Console.WriteLine();
+        }
+        public static void printEmployer(Library library)
+        {
             Console.WriteLine("Her er en list over vore medarbejder");
-            int n = 0; 
+            int n = 0;
             while (n < library.employer.Count)
             {
                 Console.WriteLine(library.employer[n].name);
