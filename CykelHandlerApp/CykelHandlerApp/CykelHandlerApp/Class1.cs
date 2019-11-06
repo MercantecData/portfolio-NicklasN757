ca212b85b76a11508512e5f0e2be1062171bde5e
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-11-06 10:46:31 +0100

    Update

---
+++ CykelHandlerApp/CykelHandlerApp/CykelHandlerApp/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CykelHandlerApp
+{
+    class Group 
+    {
+        public string name; 
+        public List<Shop> shops;
+    }
+    class Shop
+    {
+        public string name;
+        public List<Bike> bikes;
+    }
+    class Bike
+    {
+        public string color;
+        public int wheelSize;
+        public string brands;
+        public int price; 
+    }
+}
