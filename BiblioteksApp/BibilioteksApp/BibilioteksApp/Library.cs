89d414d69f3dbe99ef02f695e51c4bef461567c8
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-11-14 13:00:37 +0100

    Update BiblioteksApp

--- BiblioteksApp/BibilioteksApp/BibilioteksApp/Library.cs
+++ BiblioteksApp/BibilioteksApp/BibilioteksApp/Library.cs
@@ -6,28 +6,101 @@ namespace BibilioteksApp
 {
     class Library
     {
-        public string name;
-        public string owner;
-        public List<Employer> employer = new List<Employer>();
-        public List<Book> books = new List<Book>();
-        public List<Customer> customer = new List<Customer>();
+        private string name;
+        private string owner;
+        private List<Employer> employers = new List<Employer>();
+        private List<Book> books = new List<Book>();
+        private List<Customer> customers = new List<Customer>();
 
-        public List<Book> addBook(Book book)
+        public Library nameAdd(Library library, string name)
         {
-            books.Add(book);
-            return books;
+            library.name = name;
+            return library;
+        }
+        public Library ownerAdd(Library library, string name)
+        {
+            library.owner = name;
+            return library;
+        }
+        //Book functions
+        public Library addBook(Library library, Book book)
+        {
+            library.books.Add(book);
+            return library;
         }
 
-        public List<Book> removeBook(int bookNum)
+        public Library removeBook(Library library, Book book)
         {
-            books.RemoveAt(bookNum);
-            return books;
+            library.books.Remove(book);
+            return library;
         }
 
-        public void loanBook(Book book, Customer customer)
+        public void loanBook(Customer customer, Book book)
         {
             customer.loanBooks.Add(book);
             books.Remove(book);
         }
+        //-----------------------------------------------------//
+
+        //Customer functions
+        public Library addCustomer(Library library, Customer customer)
+        {
+            library.customers.Add(customer);
+            return library;
+        }
+        public Library removeCustomer(Library library, Customer customer)
+        {
+            library.customers.Remove(customer);
+            return library;
+        }
+        //-------------------------------------------------------//
+
+        //Employer functions
+        public Library addEmployer(Library library, Employer employer)
+        {
+            library.employers.Add(employer);
+            return library;
+        }
+        public Library removeEmployer(Library library, Employer employer)
+        {
+            library.employers.Remove(employer);
+            return library;
+        }
+        //--------------------------------------------------------//
+
+        //Get functions 
+        public string getName(Library library)
+        {
+            return library.name;
+        }
+        public string getOwner(Library library)
+        {
+            return library.owner;
+        }
+        public List<Customer> getCustomerList(Library library)
+        {
+            return library.customers;
+        }
+        public Customer getCustomer(List<Customer> customerList, int customerNum)
+        {
+            return customerList[customerNum];
+        }
+        public List<Employer> getEmployerList(Library library)
+        {
+            return library.employers;
+        }
+        public Employer getEmployer(List<Employer> employerList, int employerNum)
+        {
+            return employerList[employerNum];
+        }
+        public List<Book> getBookList(Library library)
+        {
+            return library.books;
+        }
+        public Book getBook(List<Book> bookList, int bookNum)
+        {
+            return bookList[bookNum];
+        }
+        //----------------------------------------------------//
     }
 }
