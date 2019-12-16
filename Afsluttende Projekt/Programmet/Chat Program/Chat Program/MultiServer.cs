07b3550ea8d47dab45679ea89690181c6447cb2e
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-12-16 14:46:31 +0100

    Projekt opgave start

---
+++ Afsluttende Projekt/Programmet/Chat Program/Chat Program/MultiServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Chat_Program
+{
+    class MultiServer
+    {
+        public List<TcpClient> Clients = new List<TcpClient>();
+        public MultiServer()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 13356;
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+
+            AcceptClients(listener);
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Write("Write message: ");
+                string text = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+                foreach(TcpClient client in Clients)
+                {
+                    client.GetStream().Write(buffer, 0, buffer.Length);
+                }
+            }
+
+        }
+        public async void AcceptClients(TcpListener listener)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                TcpListener client = await listener.AcceptTcpClientAsync();
+                Clients.Add(client);
+                NetworkStream stream = client.GetStream();
+                ReceiveMessages(stream);
+            }
+        }
+        public async void ReceiveMessages(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            bool isRunning = true;
+            while (isRunning)
+            {
+                int read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                String text = Encoding.UTF8.GetString(buffer, 0, read);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
---
+++ Afsluttende Projekt/Programmet/Chat Program/Chat Program/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Chat_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alle de nødvendige variabler
+            string message;
+            bool go;
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                string reply = startPrint();
+
+                if (reply == "0")
+                {
+                    isRunning = false;
+                    Console.Clear();
+                    Console.WriteLine("Programmet lukker...");
+                }
+                else if (reply == "1")
+                {
+                    Console.Clear();
+
+                    message = "Du er ved at joine en server.";
+                    go = confirmPromt(message);
+
+                    Console.Clear();
+
+                    if (go)
+                    {
+                        Console.WriteLine("It Works");
+                    }
+                }
+                else if (reply == "2")
+                {
+                    Console.Clear();
+
+                    message = "Du er ved at oprette en server.";
+                    go = confirmPromt(message);
+
+                    Console.Clear();
+
+                    if (go)
+                    {
+                        Console.WriteLine("It works");
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+        }
+        //Printer de forkellige valgmuligheder som der er i start menuen.
+        public static string startPrint()
+        {
+            Console.WriteLine("Hej og velkommen til.");
+            Console.WriteLine("0. Lukker dette program.");
+            Console.WriteLine("1. Tilslut en server (Klient).");
+            Console.WriteLine("2. Opret en server (Host).");
+            Console.Write("Vælg hvad du vil: ");
+            return Console.ReadLine();
+        }
+        //Spørger om brugeren er sikker på deres valg.
+        public static bool confirmPromt(string message)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine(message);
+                Console.WriteLine("Er du sikker på dette valg?");
+                Console.Write("Dit svar (yes/no): ");
+                string reply = Console.ReadLine().ToLower();
+                if (reply == "y" || reply == "yes")
+                {
+                    isRunning = false;
+                    return true;
+                }
+                else if (reply == "n" || reply == "no" || reply == "nope")
+                {
+                    isRunning = false;
+                    return false;
+                }
+                Console.Clear();
+            }
+            return false;
+        }
+    }
+}
